b6f3bee9c91d695ab8720e563910d26ea318c33e
Gabriel Rabelo <rabelo.g14@gmail.com>
2022-11-04 14:27:41 -0300

    acrescentando animações

--- main.js
+++ main.js
@@ -14,9 +14,11 @@ function createGame(team1, hour, team2) {
   `
 }
 
+let delay = -0.4;
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
         <ul>
             ${games}
@@ -31,6 +33,13 @@ document.querySelector("#cards").innerHTML =
     "21/11",
     "Segunda-feira",
     createGame("england", "10:00", "iran") +
-      createGame("senegal", "13:00", "netherland") +
-      createGame("usa", "16:00", "wales")
+    createGame("senegal", "13:00", "netherland") +
+    createGame("usa", "16:00", "wales")
+  ) + 
+  createCard (
+    "22/11",
+    "Terça-feira",
+    createGame("england", "07:00", "iran") +
+    createGame("senegal", "10:00", "netherland") +
+    createGame("usa", "13:00", "wales")
   )
